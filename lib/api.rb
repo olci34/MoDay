5c8d651e6f09973982f8738baed4f944b80d3867
github username <github email address>
2021-02-02 21:30:24 -0500

    defines #get_movie_by_id Api class method

--- lib/api.rb
+++ lib/api.rb
@@ -3,7 +3,9 @@ class Api
     @@api_key = "8f1dd76b"
 
     def self.get_movie_by_id(id)
-
+        url = "https://www.omdbapi.com/?i=#{id}&apikey=#{@@api_key}"
+        response = HTTParty.get(url)
+        movie_hash = {title: response["Title"], year: response["Year"], runtime: response["Runtime"]. genre_array: response["Genre"], director: response["Director"], stars: response["Actors"], plot: response["Plot"], imdbRating: response["imdbRating"]}
     end
 
 end
