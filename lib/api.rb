244faa50e47d154fee0a7d4b8f3c3a3fde3c2574
github username <github email address>
2021-02-03 14:02:54 -0500

    edits #get_movie_by_id method in Api class

--- lib/api.rb
+++ lib/api.rb
@@ -5,7 +5,9 @@ class Api
     def self.get_movie_by_id(id)
         url = "https://www.omdbapi.com/?i=#{id}&apikey=#{@@api_key}"
         response = HTTParty.get(url)
-        movie_hash = {title: response["Title"], year: response["Year"], runtime: response["Runtime"], genre_array: response["Genre"], director: response["Director"], stars: response["Actors"], plot: response["Plot"], imdbRating: response["imdbRating"]}
+        genre_array = response["Genre"].split(", ").collect {|genre_name| Genre.find_by_name(genre_name)}
+        stars = response["Actors"].split(", ").collect {|star_name| Star.find_or_create_by_name(star_name)}
+        movie_hash = {title: response["Title"], year: response["Year"], runtime: response["Runtime"], genre_array: genre_array, director: response["Director"], stars: stars, plot: response["Plot"], imdbRating: response["imdbRating"]}
         new_movie = Movie.new(movie_hash)
     end
 
