6fd1defb83138f66f63d407b04e135cef66bd134
github username <github email address>
2021-02-02 19:45:14 -0500

    defines #list_and_pick_genres method in MoDay class

---
+++ ReadMe.md
@@ -0,0 +1,16 @@
+Greets the user
+Shows the movie of the day and its details
+Lists Categories
+Asks the user to pick a category
+
+(Scrapes the top 100 movie names(or their imdbIDs) of the category from imdb
+uses scraped info to pull every movies apis
+creates movies with the api attributes)
+
+Lists random 3 movies
+asks user to pick a movie to show details
+Lists options of searching for a movie star in the category
+                 searching for a director in the category
+
+                 the user should be able to go to previous list and exit whenever they want
+
--- lib/cli.rb
+++ lib/cli.rb
@@ -1,5 +1,19 @@
 class MoDay
+
     def greeting
-        puts "Hello"
+        puts "Welcome to MoDay! Here is the movie of the day."
+        #self.movie_of_the_day Displays a random movie
+        self.list_and_pick_genres
     end
+
+    def list_and_pick_genres
+        genres = ["Action","Drama","Comedy","Documentary"] #TODO: Create Genre class
+        input = nil
+        while !(1..genres.count).include?(input) #ERROR Handling
+            puts "Please pick a genre"
+            genres.each.with_index(1) {|genre, index| puts "#{index}. #{genre}"}
+            input = gets.strip.to_i
+        end
+    end
+
 end
