457361121202613170d97469fdbdf66b75f740c8
github username <github email address>
2021-02-04 12:47:35 -0500

    switches 'colorize' gem to 'pastel' and designs colors

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,7 +1,7 @@
 class MoDay
 
     def greeting
-        puts "\n\nWelcome to MoDay! Here is the movie of the day.\n"
+        puts "\n\nWelcome to MoDay! Here is the movie of the day."
         #self.movie_of_the_day Displays a random movie
         self.list_and_pick_genres
     end
@@ -14,34 +14,41 @@ class MoDay
 
     def self.make_genre_movies(genre)
         movie_id_array = Scraper.scrape_movie_ids(genre)
-        genre_movies = movie_id_array.collect {|id| Api.get_movie_by_id(id)}
+        bar = TTY::ProgressBar.new("#{genre.name} genre content is loading[:bar]", total: 30)
+        genre_movies = movie_id_array.collect do |id|
+            Api.get_movie_by_id(id)
+            bar.advance
+        end
         genre.movies
     end
 
     def list_and_pick_genres
         Genre.all.empty? ? genres = self.class.make_genres : genres = Genre.all  
         options = genres.map {|genre| genre.name}
-        picked_genre = Genre.find_by_name(TTY::Prompt.new.select("Please pick a genre:", options))
+        picked_genre = Genre.find_by_name(TTY::Prompt.new.select(Pastel.new.cyan("\nPlease pick a genre:"), options))
+        puts "\n"
         self.list_genre_movies(picked_genre)
     end
 
     def list_genre_movies(genre)
+        pastel = Pastel.new
         prompt = TTY::Prompt.new
-        puts "\n#{genre.name} menu\n"
         genre.movies.empty? ? movies = self.class.make_genre_movies(genre) : movies = genre.movies
         suggested_movies = movies.sample(3)
 
-        selection = prompt.select("Please pick an option", self.category_options(genre))
+        selection = prompt.select(pastel.yellow("\n#{genre.name} Menu"), self.category_options(genre))
         case selection
         when self.category_options(genre)[0]
-            puts "Please enter the move star's full name: #{input = gets.strip}"
+            print pastel.cyan("Please enter the movie star's full name: ")
+            input = gets.strip
             self.input_handler(entry: input, genre: genre, object: Star, attribute: "stars")
         when self.category_options(genre)[1]
-            puts "Please enter the directors's full name: #{input = gets.strip}"
+            puts pastel.cyan("Please enter the directors's full name: ")
+            input = gets.strip
             self.input_handler(entry: input, genre: genre, object: Director, attribute: "director")
         when self.category_options(genre)[2]
             listed_movie_titles = suggested_movies.map {|movie| movie.title}
-            picked_movie = Movie.find_by_name(prompt.select("Pick a movie for more details", listed_movie_titles))
+            picked_movie = Movie.find_by_name(prompt.select(pastel.cyan("Pick a movie for more details"), listed_movie_titles))
             self.display_movie(picked_movie)
         end
     end
@@ -56,20 +63,22 @@ class MoDay
         else 
             movie_titles = instance_movies.map {|movie| movie.title}
             puts "\nHere are the #{instance.name}'s top rated #{genre.name} movies."
-            picked_movie_name = TTY::Prompt.new.select("Pick a movie for details:", movie_titles)
+            picked_movie_name = TTY::Prompt.new.select(Pastel.new.cyan("Pick a movie for details:"), movie_titles)
             picked_movie = Movie.find_by_name(picked_movie_name)
             self.display_movie(picked_movie)
         end
     end
 
     def display_movie(movie)
+        pastel = Pastel.new
         puts "\n"
-        puts "#{movie.title.colorize(:light_blue)} (#{movie.year})"
-        puts "Director(s): #{movie.director[0].name}"
+        puts "#{pastel.green.bold(movie.title)} (#{movie.year})"
+        puts "#{pastel.green.dark("Director(s)")}: #{movie.director[0].name}"
         stars_name_list = movie.stars.collect {|star| star.name}
-        puts "Movie Stars: #{stars_name_list.join(", ")}\n"
-        puts "#{movie.runtime} | imdb Rating: #{movie.imdbRating}\n"
-        puts movie.plot
+        puts "#{pastel.green.dark("Movie Stars:")} #{stars_name_list.join(", ")}\n"
+        puts "#{pastel.green.dark("Runtime:")} #{movie.runtime}"
+        puts "#{pastel.green.dark("Imdb Rating:")} #{movie.imdbRating}\n"
+        puts "#{pastel.green.dark("Plot:")} #{movie.plot}"
     end
 
     def category_options(genre)
@@ -82,7 +91,7 @@ class MoDay
 
 
     def logo
-        puts "
+        puts Pastel.new.green("
         
 ███╗   ███╗ ██████╗ ██████╗  █████╗ ██╗   ██╗
 ████╗ ████║██╔═══██╗██╔══██╗██╔══██╗╚██╗ ██╔╝
@@ -91,6 +100,6 @@ class MoDay
 ██║ ╚═╝ ██║╚██████╔╝██████╔╝██║  ██║   ██║   
 ╚═╝     ╚═╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═╝   ╚═╝   
                                              
-".colorize(:light_green)
+")
     end
 end
